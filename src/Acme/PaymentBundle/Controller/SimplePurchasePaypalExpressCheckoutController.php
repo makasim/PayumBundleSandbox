562eb55dc5a62d317f655f49751cb89b63aa6fde
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-03-13 10:29:34 +0200

    use eBook as example of digital goods.

--- src/Acme/PaymentBundle/Controller/SimplePurchasePaypalExpressCheckoutController.php
+++ src/Acme/PaymentBundle/Controller/SimplePurchasePaypalExpressCheckoutController.php
@@ -54,51 +54,49 @@ class SimplePurchasePaypalExpressCheckoutController extends Controller
 
     public function prepareDigitalGoodsAction(Request $request)
     {
-        $form = $this->createFormBuilder()
-            ->add('amount', null, array('data' => 1))
-            ->add('currency', null, array('data' => 'USD'))
-
-            ->getForm()
-        ;
+        $eBook = array(
+            'author' => 'Jules Verne', 
+            'name' => 'The Mysterious Island',
+            'description' => 'The Mysterious Island is a novel by Jules Verne, published in 1874.',
+            'price' => 2.64,
+            'currency_symbol' => '$',
+            'currency' => 'USD',
+            'quantity' => 2
+        );
 
         if ('POST' === $request->getMethod()) {
+            $paymentContext = $this->getPayum()->getContext('simple_purchase_paypal_express_checkout');
 
-            $form->bind($request);
-            if ($form->isValid()) {
-                $data = $form->getData();
+            /** @var $instruction PaymentInstruction */
+            $instruction = $paymentContext->getStorage()->createModel();
+            $instruction->setPaymentrequestCurrencycode(0, $eBook['currency']);
+            $instruction->setPaymentrequestAmt(0,  $eBook['price'] * $eBook['quantity']);
+            
+            $instruction->setNoshipping(Api::NOSHIPPING_NOT_DISPLAY_ADDRESS);
+            $instruction->setReqconfirmshipping(Api::REQCONFIRMSHIPPING_NOT_REQUIRED);
+            $instruction->setLPaymentrequestItemcategory(0, 0, Api::PAYMENTREQUEST_ITERMCATEGORY_DIGITAL);
+            $instruction->setLPaymentrequestAmt(0, 0, $eBook['price']);
+            $instruction->setLPaymentrequestQty(0, 0, $eBook['quantity']);
+            $instruction->setLPaymentrequestName(0, 0, $eBook['author'].'. '.$eBook['name']);
+            $instruction->setLPaymentrequestDesc(0, 0, $eBook['description']);
 
-                $paymentContext = $this->getPayum()->getContext('simple_purchase_paypal_express_checkout');
+            $paymentContext->getStorage()->updateModel($instruction);
+            $instruction->setInvnum($instruction->getId());
 
-                /** @var $instruction PaymentInstruction */
-                $instruction = $paymentContext->getStorage()->createModel();
-                $instruction->setPaymentrequestCurrencycode(0, $data['currency']);
-                $instruction->setPaymentrequestAmt(0,  $data['amount']);
-                $instruction->setNoshipping(Api::NOSHIPPING_NOT_DISPLAY_ADDRESS);
-                $instruction->setReqconfirmshipping(Api::REQCONFIRMSHIPPING_NOT_REQUIRED);
-                $instruction->setLPaymentrequestItemcategory(0, 0, Api::PAYMENTREQUEST_ITERMCATEGORY_DIGITAL);
-                $instruction->setLPaymentrequestAmt(0, 0, $data['amount']);
-                $instruction->setLPaymentrequestQty(0, 0, 1);
-                $instruction->setLPaymentrequestName(0, 0, 'Awesome e-book');
-                $instruction->setLPaymentrequestDesc(0, 0, 'Great stories of America.');
+            $captureUrl = $this->generateUrl('acme_payment_capture_simple', array(
+                'contextName' => 'simple_purchase_paypal_express_checkout',
+                'model' => $instruction->getId(),
+            ), $absolute = true);
+            $instruction->setReturnurl($captureUrl);
+            $instruction->setCancelurl($captureUrl);
 
-                $paymentContext->getStorage()->updateModel($instruction);
-                $instruction->setInvnum($instruction->getId());
+            $paymentContext->getStorage()->updateModel($instruction);
 
-                $captureUrl = $this->generateUrl('acme_payment_capture_simple', array(
-                    'contextName' => 'simple_purchase_paypal_express_checkout',
-                    'model' => $instruction->getId(),
-                ), $absolute = true);
-                $instruction->setReturnurl($captureUrl);
-                $instruction->setCancelurl($captureUrl);
-
-                $paymentContext->getStorage()->updateModel($instruction);
-
-                return $this->redirect($captureUrl);
-            }
+            return $this->redirect($captureUrl);
         }
 
         return $this->render('AcmePaymentBundle:SimplePurchasePaypalExpressCheckout:prepareDigitalGoods.html.twig', array(
-            'form' => $form->createView()
+            'book' => $eBook
         ));
     }
 
