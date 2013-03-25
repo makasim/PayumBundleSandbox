9c81a5783c7a22b3bf63969044785d9d1eadd3e5
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-03-25 16:23:15 +0200

    tweak recurring payment example.

--- src/Acme/PaypalExpressCheckoutBundle/Controller/RecurringPaymentExamplesController.php
+++ src/Acme/PaypalExpressCheckoutBundle/Controller/RecurringPaymentExamplesController.php
@@ -19,6 +19,19 @@ use Payum\Paypal\ExpressCheckout\Nvp\Api;
 class RecurringPaymentExamplesController extends Controller
 {
     /**
+     * @return array
+     */
+    protected function getWeatherForecastSubscriptionDetails()
+    {
+        return array(
+            'description' => 'Subscribe to the weather forecast for a week. It is 0.05$ per day.',
+            'price' => 0.05,
+            'currency' => 'USD',
+            'frequency' => 7
+        );
+    }
+
+    /**
      * @Extra\Route(
      *   "/prepare_recurring_payment",
      *   name="acme_paypal_express_checkout_prepare_recurring_payment"
@@ -28,40 +41,35 @@ class RecurringPaymentExamplesController extends Controller
      */
     public function prepareAction(Request $request)
     {
-        $form = $this->createBillingAgreementForm();
+        $subscription = $this->getWeatherForecastSubscriptionDetails();
         
-        if ($request->isMethod('POST')) {
-            $form->bind($request);
-            if ($form->isValid()) {
-                $data = $form->getData();
-                
-                $paymentContext = $this->getPayum()->getContext('simple_recurring_payment_paypal_express_checkout');
+        if ($request->isMethod('POST')) {                
+            $paymentContext = $this->getPayum()->getContext('simple_recurring_payment_paypal_express_checkout');
 
-                /** @var $billingAgreementDetails PaymentDetails */
-                $billingAgreementDetails = $paymentContext->getStorage()->createModel();
-                $billingAgreementDetails->setPaymentrequestAmt(0,  $amount = 0);
-                $billingAgreementDetails->setLBillingtype(0, Api::BILLINGTYPE_RECURRING_PAYMENTS);
-                $billingAgreementDetails->setLBillingagreementdescription(0, $data['billing_agreement_description']);
-                $billingAgreementDetails->setNoshipping(1);
-                
-                $paymentContext->getStorage()->updateModel($billingAgreementDetails);
-                $billingAgreementDetails->setInvnum($billingAgreementDetails->getId());
-        
-                $captureUrl = $this->generateUrl('acme_paypal_express_checkout_create_recurring_payment', array(
-                    'contextName' => 'simple_recurring_payment_paypal_express_checkout',
-                    'billingAgreementDetails' => $billingAgreementDetails->getId(),
-                ), $absolute = true);
-                $billingAgreementDetails->setReturnurl($captureUrl);
-                $billingAgreementDetails->setCancelurl($captureUrl);
-        
-                $paymentContext->getStorage()->updateModel($billingAgreementDetails);
+            /** @var $billingAgreementDetails PaymentDetails */
+            $billingAgreementDetails = $paymentContext->getStorage()->createModel();
+            $billingAgreementDetails->setPaymentrequestAmt(0,  $amount = 0);
+            $billingAgreementDetails->setLBillingtype(0, Api::BILLINGTYPE_RECURRING_PAYMENTS);
+            $billingAgreementDetails->setLBillingagreementdescription(0, $subscription['description']);
+            $billingAgreementDetails->setNoshipping(1);
+            
+            $paymentContext->getStorage()->updateModel($billingAgreementDetails);
+            $billingAgreementDetails->setInvnum($billingAgreementDetails->getId());
+    
+            $captureUrl = $this->generateUrl('acme_paypal_express_checkout_create_recurring_payment', array(
+                'contextName' => 'simple_recurring_payment_paypal_express_checkout',
+                'billingAgreementDetails' => $billingAgreementDetails->getId(),
+            ), $absolute = true);
+            $billingAgreementDetails->setReturnurl($captureUrl);
+            $billingAgreementDetails->setCancelurl($captureUrl);
+    
+            $paymentContext->getStorage()->updateModel($billingAgreementDetails);
 
-                return $this->redirect($captureUrl);
-            }
+            return $this->redirect($captureUrl);
         }
         
         return array(
-            'form' => $form->createView()
+            'subscription' => $subscription
         );
     }
 
@@ -85,17 +93,18 @@ class RecurringPaymentExamplesController extends Controller
 
         $recurringPaymentStatus = null;
         if ($billingAgreementStatus->isSuccess()) {
+            $subscription = $this->getWeatherForecastSubscriptionDetails();
             $billingAgreementDetails = $billingAgreementStatus->getModel();
             
             $recurringPaymentDetails = new RecurringPaymentDetails();
             $recurringPaymentDetails->setToken($billingAgreementDetails->getToken());
-            $recurringPaymentDetails->setProfilestartdate(date(DATE_ATOM));
             $recurringPaymentDetails->setDesc($billingAgreementDetails->getLBillingagreementdescription(0));
-            $recurringPaymentDetails->setAmt(1.45);
-            $recurringPaymentDetails->setCurrencycode('USD');
-            $recurringPaymentDetails->setBillingperiod(Api::BILLINGPERIOD_DAY);
-            $recurringPaymentDetails->setBillingfrequency(2);
             $recurringPaymentDetails->setEmail($billingAgreementDetails->getEmail());
+            $recurringPaymentDetails->setAmt($subscription['price']);
+            $recurringPaymentDetails->setCurrencycode($subscription['currency']);
+            $recurringPaymentDetails->setBillingfrequency($subscription['frequency']);
+            $recurringPaymentDetails->setProfilestartdate(date(DATE_ATOM));
+            $recurringPaymentDetails->setBillingperiod(Api::BILLINGPERIOD_DAY);
             
             $context->getPayment()->execute(new CreateRecurringPaymentProfileRequest($recurringPaymentDetails));
             $context->getPayment()->execute(new GetRecurringPaymentsProfileDetailsRequest($recurringPaymentDetails));
@@ -108,22 +117,6 @@ class RecurringPaymentExamplesController extends Controller
             'billingAgreementStatus' => $billingAgreementStatus,
             'recurringPaymentStatus' => $recurringPaymentStatus,
         );
-    }
-
-    /**
-     * @return \Symfony\Component\Form\Form
-     */
-    protected function createBillingAgreementForm()
-    {
-        return $this->createFormBuilder()
-            ->add('billing_agreement_description', null, array(
-                'data' => 'Subscribe for whether forecast',
-                'label' => 'Subscription desc: ',
-                'constraints' => new NotBlank
-            ))
-    
-            ->getForm()
-        ;
     }
     
     /**
