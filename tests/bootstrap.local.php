fb8e471cf1088a3c8a455e9757de5ce231ce9c5e
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-04-01 22:35:58 +0300

    [test] add bootstrap test to repo.

---
+++ tests/bootstrap.local.php
@@ -0,0 +1,24 @@
+<?php
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    echo <<<EOM
+You must set up the project dependencies by running the following commands:
+
+    curl -s http://getcomposer.org/installer | php
+    php composer.phar install
+
+EOM;
+
+    exit(1);
+}
+
+$loader->add('Payum\Tests', realpath(__DIR__ .'/../vendor/payum/payum/tests'));
+$loader->add('Payum\Examples', realpath(__DIR__ .'/../vendor/payum/payum/examples'));
+
+$loader->add('Payum\Paypal\ExpressCheckout\Nvp\Examples', realpath(__DIR__ .'/../vendor/payum/paypal-express-checkout-nvp/examples'));
+$loader->add('Payum\Paypal\ExpressCheckout\Nvp\Tests', realpath(__DIR__ .'/../vendor/payum/paypal-express-checkout-nvp/tests'));
+
+$loader->add('Payum\Paypal\AuthorizeNet\Aim\Tests', realpath(__DIR__ .'/../vendor/payum/authorize-net-aim/tests'));
+
+$loader->add('Payum\Paypal\Be2Bill\Tests', realpath(__DIR__ .'/../vendor/payum/be2bill/tests'));
+
+$loader->add('Payum\Bundle\PayumBundle\Tests', __DIR__);
