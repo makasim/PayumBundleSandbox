569fe0b630540f9319bc47f058536a639975f287
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2013-05-12 17:29:42 +0300

    generate other example bundle.

---
+++ src/Acme/OtherExamplesBundle/Controller/CartExamplesController.php
@@ -0,0 +1,206 @@
+<?php
+namespace Acme\OtherExamplesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Range;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as Extra;
+
+use Payum\Registry\AbstractRegistry;
+use Payum\Paypal\ExpressCheckout\Nvp\Api;
+use Payum\Paypal\ExpressCheckout\Nvp\Model\PaymentDetails;
+use Payum\Bundle\PayumBundle\Service\TokenizedTokenService;
+
+class CartExamplesController extends Controller
+{
+    /**
+     * @Extra\Route(
+     *   "/select_payment", 
+     *   name="acme_other_example_select_payment"
+     * )
+     * 
+     * @Extra\Template
+     */
+    public function prepareAction(Request $request)
+    {
+        $form = $this->createPurchaseForm();
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+                
+                $storage = $this->getPayum()->getStorageForClass(
+                    'Acme\PaypalExpressCheckoutBundle\Model\PaymentDetails',
+                    $paymentName
+                );
+
+                /** @var $paymentDetails PaymentDetails */
+                $paymentDetails = $storage->createModel();
+                $paymentDetails->setPaymentrequestCurrencycode(0, $data['currency']);
+                $paymentDetails->setPaymentrequestAmt(0,  $data['amount']);
+                $storage->updateModel($paymentDetails);
+                
+                $captureToken = $this->getTokenizedTokenService()->createTokenForCaptureRoute(
+                    $paymentName,
+                    $paymentDetails,
+                    'acme_payment_details_view'
+                );
+                
+                $paymentDetails->setReturnurl($captureToken->getTargetUrl());
+                $paymentDetails->setCancelurl($captureToken->getTargetUrl());
+                $paymentDetails->setInvnum($paymentDetails->getId());
+                $storage->updateModel($paymentDetails);
+
+                return $this->redirect($captureToken->getTargetUrl());
+            }
+        }
+        
+        return array(
+            'form' => $form->createView()
+        );
+    }
+
+    /**
+     * @Extra\Route(
+     *   "/repare_simple_purchase_and_doctrine",
+     *   name="acme_paypal_express_checkout_prepare_simple_purchase_and_doctrine"
+     * )
+     * 
+     * @Extra\Template
+     */
+    public function prepareDoctrineAction(Request $request)
+    {
+        $paymentName = 'paypal_express_checkout_plus_doctrine';
+        
+        $form = $this->createPurchaseForm();
+        if ('POST' === $request->getMethod()) {
+            $form->bind($request);
+            if ($form->isValid()) {
+                $data = $form->getData();
+
+                $storage = $this->getPayum()->getStorageForClass(
+                    'Acme\PaypalExpressCheckoutBundle\Entity\PaymentDetails',
+                    $paymentName
+                );
+                
+                /** @var $paymentDetails PaymentDetails */
+                $paymentDetails = $storage->createModel();
+                $paymentDetails->setPaymentrequestCurrencycode(0, $data['currency']);
+                $paymentDetails->setPaymentrequestAmt(0,  $data['amount']);
+
+                $storage->updateModel($paymentDetails);
+
+                $captureToken = $this->getTokenizedTokenService()->createTokenForCaptureRoute(
+                    $paymentName,
+                    $paymentDetails,
+                    'acme_payment_details_view'
+                );
+
+                $paymentDetails->setReturnurl($captureToken->getTargetUrl());
+                $paymentDetails->setCancelurl($captureToken->getTargetUrl());
+                $paymentDetails->setInvnum($paymentDetails->getId());
+                $storage->updateModel($paymentDetails);
+
+                return $this->redirect($captureToken->getTargetUrl());
+            }
+        }
+
+        return array(
+            'form' => $form->createView()
+        );
+    }
+
+    /**
+     * @Extra\Route(
+     *   "/digital_goods_purchase",
+     *   name="acme_paypal_express_checkout_prepare_digital_goods_purchase"
+     * )
+     * 
+     * @Extra\Template
+     */
+    public function prepareDigitalGoodsAction(Request $request)
+    {
+        $paymentName = 'paypal_express_checkout';
+        
+        $eBook = array(
+            'author' => 'Jules Verne', 
+            'name' => 'The Mysterious Island',
+            'description' => 'The Mysterious Island is a novel by Jules Verne, published in 1874.',
+            'price' => 2.64,
+            'currency_symbol' => '$',
+            'currency' => 'USD',
+            'quantity' => 2
+        );
+
+        if ('POST' === $request->getMethod()) {
+            $storage = $this->getPayum()->getStorageForClass(
+                'Acme\PaypalExpressCheckoutBundle\Model\PaymentDetails',
+                $paymentName
+            );
+
+            /** @var $paymentDetails PaymentDetails */
+            $paymentDetails = $storage->createModel();
+            $paymentDetails->setPaymentrequestCurrencycode(0, $eBook['currency']);
+            $paymentDetails->setPaymentrequestAmt(0,  $eBook['price'] * $eBook['quantity']);
+            
+            $paymentDetails->setNoshipping(Api::NOSHIPPING_NOT_DISPLAY_ADDRESS);
+            $paymentDetails->setReqconfirmshipping(Api::REQCONFIRMSHIPPING_NOT_REQUIRED);
+            $paymentDetails->setLPaymentrequestItemcategory(0, 0, Api::PAYMENTREQUEST_ITERMCATEGORY_DIGITAL);
+            $paymentDetails->setLPaymentrequestAmt(0, 0, $eBook['price']);
+            $paymentDetails->setLPaymentrequestQty(0, 0, $eBook['quantity']);
+            $paymentDetails->setLPaymentrequestName(0, 0, $eBook['author'].'. '.$eBook['name']);
+            $paymentDetails->setLPaymentrequestDesc(0, 0, $eBook['description']);
+
+            $storage->updateModel($paymentDetails);
+
+            $captureToken = $this->getTokenizedTokenService()->createTokenForCaptureRoute(
+                $paymentName,
+                $paymentDetails,
+                'acme_payment_details_view'
+            );
+
+            $paymentDetails->setReturnurl($captureToken->getTargetUrl());
+            $paymentDetails->setCancelurl($captureToken->getTargetUrl());
+            $paymentDetails->setInvnum($paymentDetails->getId());
+            $storage->updateModel($paymentDetails);
+
+            return $this->redirect($captureToken->getTargetUrl());
+        }
+
+        return array(
+            'book' => $eBook
+        );
+    }
+
+    /**
+     * @return \Symfony\Component\Form\Form
+     */
+    protected function createPurchaseForm()
+    {
+        return $this->createFormBuilder()
+            ->add('amount', null, array(
+                'data' => 1,
+                'constraints' => array(new Range(array('max' => 2)))
+            ))
+            ->add('currency', null, array('data' => 'USD'))
+            ->getForm()
+        ;
+    }
+
+    /**
+     * @return AbstractRegistry
+     */
+    protected function getPayum()
+    {
+        return $this->get('payum');
+    }
+
+    /**
+     * @return TokenizedTokenService
+     */
+    protected function getTokenizedTokenService()
+    {
+        return $this->get('payum.tokenized_details_service');
+    }
+}
