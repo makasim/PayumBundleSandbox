ece42ae0918e59ee69f4dcbba4fd2539b2d8d9c2
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2015-02-06 09:26:02 +0000

    use index.php + env vars.

---
+++ web/index.php
@@ -0,0 +1,21 @@
+<?php
+
+if (false == isset($_SERVER['SYMFONY_ENV'], $_SERVER['SYMFONY_DEBUG'])) {
+    http_response_code(500);
+    die('Internal error. The environment is not configured proper way.');
+}
+$env = (string) $_SERVER['SYMFONY_ENV'];
+$debug = (bool) $_SERVER['SYMFONY_DEBUG'];
+
+use Symfony\Component\HttpFoundation\Request;
+
+$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+require_once __DIR__.'/../app/AppKernel.php';
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
