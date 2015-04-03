a4aac2b9e7ea4d26f3fad7b608f21bb0583398bf
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2015-04-03 12:48:52 +0000

    [example] add an example of basic usage of Paylink payment gateway.

--- src/Acme/OtherExamplesBundle/AcmeOtherExamplesBundle.php
+++ src/Acme/OtherExamplesBundle/AcmeOtherExamplesBundle.php
@@ -3,6 +3,7 @@
 namespace Acme\OtherExamplesBundle;
 
 use Acme\OtherExamplesBundle\Payum\FooBarGateway\FooBarGatewayPaymentFactory;
+use Locastic\PayLinkPayum\Bridge\Symfony\PaylinkPaymentFactory;
 use Payum\Bridge\JMSPayment\DependencyInjection\Factory\Payment\JMSPaymentPaymentFactory;
 use Payum\Bundle\PayumBundle\DependencyInjection\PayumExtension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -18,7 +19,9 @@ class AcmeOtherExamplesBundle extends Bundle
         /** @var  PayumExtension $payumExtension */
         $payumExtension = $container->getExtension('payum');
 
-        $payumExtension->addPaymentFactory(new JMSPaymentPaymentFactory);
-        $payumExtension->addPaymentFactory(new FooBarGatewayPaymentFactory);
+        $payumExtension->addPaymentFactory(new JMSPaymentPaymentFactory());
+        $payumExtension->addPaymentFactory(new FooBarGatewayPaymentFactory());
+
+        $payumExtension->addPaymentFactory(new PaylinkPaymentFactory());
     }
 }
