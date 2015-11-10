56e4c5cc9edcddd894d237efbd5a45fefaa5e767
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2015-11-10 04:08:00 -0500

    remove hard coded dev env

--- web/index.php
+++ web/index.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 
-$kernel = new AppKernel('dev', true);
+$kernel = new AppKernel($env, $debug);
 $kernel->loadClassCache();
 Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
