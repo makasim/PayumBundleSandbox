1d3c11583587524c20a64989b861a720a27fd1ec
Dmitry <hellsigner@gmail.com>
2016-03-30 19:54:08 +0300

    Customized new ApiViaToken class to use AuthToken for authorization, but it's not working properly yet

--- src/Acme/PaymentBundle/Payum/Paypal/ExpressCheckout/Nvp/ApiViaToken.php
+++ src/Acme/PaymentBundle/Payum/Paypal/ExpressCheckout/Nvp/ApiViaToken.php
@@ -2,8 +2,11 @@
 
 namespace Acme\PaymentBundle\Payum\Paypal\ExpressCheckout\Nvp;
 
+use PayPal\Auth\Oauth\AuthSignature;
+use Payum\Core\Exception\Http\HttpException;
 use Payum\Paypal\ExpressCheckout\Nvp;
 use Payum\Paypal\ExpressCheckout\Nvp\Api;
+use GuzzleHttp\Psr7\Request;
 
 
 /**
@@ -13,5 +16,76 @@ use Payum\Paypal\ExpressCheckout\Nvp\Api;
  */
 class ApiViaToken extends Api
 {
+
+    protected $options = array(
+        'username' => null,
+        'password' => null,
+        'signature' => null,
+        'return_url' => null,
+        'cancel_url' => null,
+        'sandbox' => null,
+        'useraction' => null,
+        'token' => null,
+        'tokenSecret' => null,
+        'cmd' => Api::CMD_EXPRESS_CHECKOUT,
+    );
+
+    /**
+     * @param array $fields
+     *
+     * @throws HttpException
+     *
+     * @return array
+     */
+    protected function doRequest(array $fields)
+    {
+        $headers = array(
+            'Content-Type' => 'application/x-www-form-urlencoded',
+        );
+
+        $method = 'POST';
+        $this->addAuthorizeHeader($headers, $method);
+
+        $request = new Request($method, $this->getApiEndpoint(), $headers, http_build_query($fields));
+
+        $response = $this->client->send($request);
+
+        if (false == ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300)) {
+            throw HttpException::factory($request, $response);
+        }
+
+        $result = array();
+        parse_str($response->getBody()->getContents(), $result);
+        foreach ($result as &$value) {
+            $value = urldecode($value);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $fields
+     */
+    protected function addAuthorizeFields(array &$fields)
+    {
+        // we add nothing here; instead we use headers for this auth type
+    }
+
+    protected function addAuthorizeHeader(array &$headers, $method = 'POST')
+    {
+        $authSignature = AuthSignature::generateFullAuthString(
+            $this->options['username'],
+            $this->options['password'],
+            $this->options['token'],
+            $this->options['tokenSecret'],
+            $method,
+            $this->getApiEndpoint()
+        );
+
+//        var_dump($this->options);
+//        die($authSignature);
+
+        $headers['X-PAYPAL-AUTHORIZATION'] = $authSignature;
+    }
 
 }
