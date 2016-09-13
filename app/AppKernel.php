1c37f646ee5680ec393f5513a3652764ee7a5737
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2016-09-13 22:37:09 +0300

    tcompayway example

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -47,6 +47,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new Locastic\TcomPayWayPayumBundle\LocasticTcomPayWayPayumBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
